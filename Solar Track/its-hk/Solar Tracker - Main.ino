58fda58f57fa6d7a7e38ce30d32083c03716a9e6
kasun <harshana.kasun003@gmail.com>
2023-11-09 12:52:18 +0000

    its-hk/Solar Tracker Code: Clean up
    Code Clean up

--- Solar Track/its-hk/Solar Tracker - Main.ino
+++ Solar Track/its-hk/Solar Tracker - Main.ino
@@ -6,15 +6,15 @@ int X1Y1LDR;
 int X1Y2LDR;
 int X2Y1LDR;
 int X2Y2LDR;
-int tavg;
-int diff;
+int voltage;
+int current;
 int divisor;
 int spd;
 int sensitivity;
-#X1 = left 
-#X2 = RIGHT
-#Y1 = UP
-#Y2 = DOWN
+//X1 = left 
+//X2 = RIGHT
+//Y1 = UP
+//Y2 = DOWN
 
 
 
@@ -26,16 +26,10 @@ void setup () {
   sensitivity = 5; // this controls the sensitivity of the tracker. lower number = higher sensitivity. if your tracker is constantly jittering back and forth increase the number
   Serial.begin(19200); // open serial com
   Serial.print("Solar Tracker ready!");
-  //pinMode(BOTTOM, INPUT); // set the inputs
-  //pinMode(TOPLEFT, INPUT);
-  //pinMode(TOPRIGHT, INPUT);
-  //pinMode(BOTRIGHT, INPUT);
-  //pinMode(BOTLEFT, INPUT);
 }
 
 void loop () {
-tiltl = digitalRead(TILTL); // read the tilt sensor
-tilth = digitalRead(TILTH);
+
 x1y1sense = analogRead(TOPLEFT); // read the light sensors
 X2y1 = analogRead(TOPRIGHT);
 x1y2 = analogRead(BOTLEFT); // read the light sensors
@@ -45,10 +39,6 @@ avgX1 = (x1y2 + x1y1)/2
 avgX2 = (x2y2+x2y1)/2
 avgY1 = (x1y1+x2Y1)/2
 avgY2 = (x1y2+x2y2)/2
-
-
-
-
 //tavg = (tlsense + trsense)/2; // get an average value for the top 2 sensors
 //diff = abs(tavg - bsense); // this judges how far the tracker must turn
 spd = diff/divisor; // and adjusts the speed of the reaction accordingly
@@ -57,39 +47,6 @@ Serial.print("\nTOP: "); Serial.print(tavg, DEC); // print the sensor values to 
 Serial.print("\tBOTTOM:"); Serial.print(bsense, DEC);
 Serial.print("\tLEFT:"); Serial.print(tlsense, DEC);
 Serial.print("\tRIGHT:"); Serial.print(trsense, DEC);
-if((tavg < bsense) && (diff > sensitivity) && (tiltl == LOW) && (tilth == LOW)){ // if the average value of the top sensors is smaller (more light) than the bottom sensor and the tilt sensor is in the correct range
-vservo.write(90 - spd); // send servo command to turn upward plus add speed
-Serial.print("\tState: "); Serial.print("UP!");
-}else if((tavg < bsense) && (diff > sensitivity) && (tiltl == HIGH) && (tilth == LOW)){ // if the average value of the top sensors is smaller (more light) than the bottom sensor and the tilt sensor is in the correct range
-vservo.write(90 - spd); // send servo command to turn upward plus add speed
-Serial.print("\tState: "); Serial.print("UP!");
-}else if((tavg > bsense) && (diff > sensitivity) && (tiltl == HIGH) && (tilth == LOW)){ // if the value of the bottom sensor is smaller (more light) than the average value of the top sensors and the tilt sensor is in the correct range
-vservo.write(90 + spd); // send servo command to turn downward plus add speed
-Serial.print("\tState: "); Serial.print("DOWN!");
-}else if((tavg > bsense) && (diff > sensitivity) && (tiltl == LOW) && (tilth == HIGH)){ // if the value of the bottom sensor is smaller (more light) than the average value of the top sensors and the tilt sensor is in the correct range
-vservo.write(90 + spd); // send servo command to turn downward plus add speed
-Serial.print("\tState: "); Serial.print("DOWN!");
-}else{ // for every other instance
-vservo.write(90); // stop the y-axis motor
-Serial.print("\tState: "); Serial.print("STOP!");
 }
-tlsense = analogRead(TOPLEFT); // read the top 2 sensors again because they have probably changed
-trsense = analogRead(TOPRIGHT);
-//trsense = trsense * 1.03; // again I had to adjust the value of one sensor to make the tracker more accurate
-diff = abs(tlsense - trsense); // reset the diff variable for the new values
-spd = diff/divisor; // and generate a speed accordingly
-spd = max(spd, 1); // set the minimum speed to 1
-if((tlsense < trsense) && (diff > sensitivity)){ // if the top left sensor value is smaller (more light) than the top right sensor
-hservo.write(90 + spd); // send servo command to turn left
-Serial.print("\tState: "); Serial.print("LEFT!");
-}else if((tlsense > trsense) && (diff > sensitivity)){ // if the top left sensor value is larger (less light) than the top right sensor
-hservo.write(90 - spd); // send servo command to turn right
-Serial.print("\tState: "); Serial.print("RIGHT!");
-}else{ // for every other instance
-hservo.write(90); // stop the x-axis motor
-Serial.print("\tState: "); Serial.print("STOP!");
-}
-delay(10); // delay 10ms
-Turning is 90 is toward left antilock wise
-If x1y1 > x2y1 
+
 
