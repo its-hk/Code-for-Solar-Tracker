b0d6c92a017dfda682b0b02618389d7845d219b9
Zino <150347833+G3EN0@users.noreply.github.com>
2023-11-10 09:09:41 +0000

    changed void loop

--- Solar Track/G3EN0/Solar Tracker - Main.ino
+++ Solar Track/G3EN0/Solar Tracker - Main.ino
@@ -26,20 +26,18 @@ void setup () {
   sensitivity = 5; // this controls the sensitivity of the tracker. lower number = higher sensitivity. if your tracker is constantly jittering back and forth increase the number
   Serial.begin(19200); // open serial com
   Serial.print("SolarTracker ready!");
-  //pinMode(BOTTOM, INPUT); // set the inputs
-  //pinMode(TOPLEFT, INPUT);
-  //pinMode(TOPRIGHT, INPUT);
-  //pinMode(BOTRIGHT, INPUT);
-  //pinMode(BOTLEFT, INPUT);
+  pinMode(x1y1, INPUT);
+  pinMode(x1y2, INPUT);
+  pinMode(x2xy1, INPUT);
+  pinMode(x2y2, INPUT);
 }
 void loop () {
-tiltl = digitalRead(TILTL); // read the tilt sensor
-tilth = digitalRead(TILTH);
-x1y1sense = analogRead(TOPLEFT); // read the light sensors
+voltage = analogRead(TILTL); // read the tilt sensor
+current = analogRead(TILTH);
+x1y1 = analogRead(TOPLEFT); // read the light sensors
 X2y1 = analogRead(TOPRIGHT);
 x1y2 = analogRead(BOTLEFT); // read the light sensors
 X2y2 = analogRead(BOTRIGHT);
-//bsense = bsense * 1.05; // I had to adjust the value of this sensor to make it more accurate. you might have to do the same but start by leaving it alone
 avgX1 = (x1y2 + x1y1)/2
 avgX2 = (x2y2+x2y1)/2
 avgY1 = (x1y1+x2Y1)/2
